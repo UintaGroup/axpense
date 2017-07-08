9945e0bc061de62c7a29e3b8cb626af8a9843adb
Stone C. Lasley <stonelasley@gmail.com>
2017-07-08 12:19:16 -0600

    linted tutorial-page.ts

--- src/pages/tutorial/tutorial.ts
+++ src/pages/tutorial/tutorial.ts
@@ -1,73 +1,68 @@
-import { Component } from '@angular/core';
-
-import { MenuController, NavController } from 'ionic-angular';
-
-import { WelcomePage } from '../welcome/welcome';
-
-import { TranslateService } from '@ngx-translate/core';
-
-
+import { Component }                                from '@angular/core';
+import { MenuController, NavController, Slides }    from 'ionic-angular';
+import { TranslateService }                         from '@ngx-translate/core';
+import { WelcomePage }                              from '../welcome/welcome';
 
 export interface Slide {
-  title: string;
-  description: string;
-  image: string;
+	title: string;
+	description: string;
+	image: string;
 }
 
 @Component({
-  selector: 'page-tutorial',
-  templateUrl: 'tutorial.html'
+	selector: 'page-tutorial',
+	templateUrl: 'tutorial.html'
 })
 export class TutorialPage {
-  slides: Slide[];
-  showSkip = true;
+	public slides: Slide[];
+	public showSkip: boolean = true;
 
-  constructor(public navCtrl: NavController, public menu: MenuController, translate: TranslateService) {
-    translate.get(['CONTENT.TUTORIAL.SLIDE1_TITLE',
-                   'CONTENT.TUTORIAL.SLIDE1_DESCRIPTION',
-                   'CONTENT.TUTORIAL.SLIDE2_TITLE',
-                   'CONTENT.TUTORIAL.SLIDE2_DESCRIPTION',
-                   'CONTENT.TUTORIAL.SLIDE3_TITLE',
-                   'CONTENT.TUTORIAL.SLIDE3_DESCRIPTION',
-    ])
-    .subscribe(values => {
-      this.slides = [
-        {
-          title: values['CONTENT.TUTORIAL.SLIDE1_TITLE'],
-          description: values['CONTENT.TUTORIAL.SLIDE1_DESCRIPTION'],
-          image: 'assets/img/ica-slidebox-img-1.png',
-        },
-        {
-          title: values['CONTENT.TUTORIAL.SLIDE2_TITLE'],
-          description: values['CONTENT.TUTORIAL.SLIDE2_DESCRIPTION'],
-          image: 'assets/img/ica-slidebox-img-2.png',
-        },
-        {
-          title: values['CONTENT.TUTORIAL.SLIDE3_TITLE'],
-          description: values['CONTENT.TUTORIAL.SLIDE3_DESCRIPTION'],
-          image: 'assets/img/ica-slidebox-img-3.png',
-        }
-      ];
-    });
-  }
+	constructor(private _navCtrl: NavController, private _menuCtrl: MenuController, translate: TranslateService) {
+		translate.get(['CONTENT.TUTORIAL.SLIDE1_TITLE',
+				'CONTENT.TUTORIAL.SLIDE1_DESCRIPTION',
+				'CONTENT.TUTORIAL.SLIDE2_TITLE',
+				'CONTENT.TUTORIAL.SLIDE2_DESCRIPTION',
+				'CONTENT.TUTORIAL.SLIDE3_TITLE',
+				'CONTENT.TUTORIAL.SLIDE3_DESCRIPTION',
+			])
+			.subscribe(values => {
+				this.slides = [
+					{
+						title: values['CONTENT.TUTORIAL.SLIDE1_TITLE'],
+						description: values['CONTENT.TUTORIAL.SLIDE1_DESCRIPTION'],
+						image: 'assets/img/ica-slidebox-img-1.png',
+					},
+					{
+						title: values['CONTENT.TUTORIAL.SLIDE2_TITLE'],
+						description: values['CONTENT.TUTORIAL.SLIDE2_DESCRIPTION'],
+						image: 'assets/img/ica-slidebox-img-2.png',
+					},
+					{
+						title: values['CONTENT.TUTORIAL.SLIDE3_TITLE'],
+						description: values['CONTENT.TUTORIAL.SLIDE3_DESCRIPTION'],
+						image: 'assets/img/ica-slidebox-img-3.png',
+					}
+				];
+			});
+	}
 
-  startApp() {
-    this.navCtrl.setRoot(WelcomePage, {}, {
-      animate: true,
-      direction: 'forward'
-    });
-  }
+	public startApp(): Promise<any> {
+		return this._navCtrl.setRoot(WelcomePage, {}, {
+			animate: true,
+			direction: 'forward'
+		});
+	}
 
-  onSlideChangeStart(slider) {
-    this.showSkip = !slider.isEnd;
-  }
+	public onSlideChangeStart(slider: Slides): void {
+		this.showSkip = !slider.isEnd;
+	}
 
-  ionViewDidEnter() {
-    this.menu.enable(false);
-  }
+	public ionViewDidEnter(): any {
+		this._menuCtrl.enable(false);
+	}
 
-  ionViewWillLeave() {
-    this.menu.enable(true);
-  }
+	public ionViewWillLeave(): any {
+		this._menuCtrl.enable(true);
+	}
 
 }
