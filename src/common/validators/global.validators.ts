fa103848a842a70e1de3335cca89da956e86b6c0
Stone C. Lasley <stonelasley@gmail.com>
2017-07-08 12:47:59 -0600

    lint global.validators

--- src/common/validators/global.validators.ts
+++ src/common/validators/global.validators.ts
@@ -1,16 +1,18 @@
 import { AbstractControl }  from '@angular/forms';
 import { Observable }       from 'rxjs/Observable';
 
-const EMAIL_REGEXP = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
+const EMAIL_REGEXP: any = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
 
 export class GlobalValidator {
 
-	static mailFormat(control: AbstractControl): Observable<ValidationResult> {
+	public static mailFormat(control: AbstractControl): Observable<ValidationResult> {
 		return new Observable(observer => {
-			let result = null;
+			let result: any = null;
+			/* tslint:disable */
 			if (control.value != '' && (control.value.length <= 5 || !EMAIL_REGEXP.test(control.value))) {
 				result = {'incorrectMailFormat': true};
 			}
+			/* tslint:enable */
 			observer.next(result);
 		});
 	}
