4f8ff3f2e39a25a46d8d5fe95590109b0f8c7a2e
Stone C. Lasley <stonelasley@gmail.com>
2017-07-08 13:47:37 -0600

    Show html progress

--- karma.conf.js
+++ karma.conf.js
@@ -9,6 +9,7 @@ module.exports = function (config) {
 			require('karma-jasmine'),
 			require('karma-chrome-launcher'),
 			require('karma-mocha-reporter'),
+			require('karma-jasmine-html-reporter'),
 			require('karma-coverage-istanbul-reporter'),
 			require('@angular/cli/plugins/karma')
 		],
@@ -34,7 +35,9 @@ module.exports = function (config) {
 			config: './angular-cli.json',
 			environment: 'dev'
 		},
-		reporters: ['mocha', 'coverage-istanbul'],
+		reporters: config.angularCli && config.angularCli.codeCoverage
+			? ['progress', 'coverage-istanbul']
+			: ['progress', 'kjhtml'],
 		port: 9876,
 		colors: true,
 		logLevel: config.LOG_INFO,
