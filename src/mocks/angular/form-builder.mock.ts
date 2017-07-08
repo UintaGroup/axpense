db0aaa1dba0e34fb09ca5e2cdde9feed255b94fd
Stone C. Lasley <stonelasley@gmail.com>
2017-07-08 12:59:13 -0600

    Linted FormBuilderMock

--- src/mocks/angular/form-builder.mock.ts
+++ src/mocks/angular/form-builder.mock.ts
@@ -1,9 +1,9 @@
 import { FormGroupMock } from './form-group.mock';
 
 export class FormBuilderMock {
-	public static instance(form?: FormGroupMock): any{
+	public static instance(form?: FormGroupMock): any {
 
-		let instance = jasmine.createSpyObj('FormBuilder', ['group']);
+		let instance: any = jasmine.createSpyObj('FormBuilder', ['group']);
 		instance.group.and.returnValue(form || FormGroupMock.instance());
 
 		return instance;
