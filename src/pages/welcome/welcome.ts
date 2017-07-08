42e498a270337d63e5cfd7f484a7e518539fbea7
Stone C. Lasley <stonelasley@gmail.com>
2017-07-08 12:32:10 -0600

    lint welcome page

--- src/pages/welcome/welcome.ts
+++ src/pages/welcome/welcome.ts
@@ -1,28 +1,23 @@
-import { Component } from '@angular/core';
+import { Component }     from '@angular/core';
 import { NavController } from 'ionic-angular';
 
-import { LoginPage } from '../login/login';
-import { SignupPage } from '../signup/signup';
+import { LoginPage }     from '../login/login';
+import { SignupPage }    from '../signup/signup';
 
-/**
- * The Welcome Page is a splash page that quickly describes the app,
- * and then directs the user to create an account or log in.
- * If you'd like to immediately put the user onto a login/signup page,
- * we recommend not using the Welcome page.
-*/
 @Component({
-  selector: 'page-welcome',
-  templateUrl: 'welcome.html'
+	selector: 'page-welcome',
+	templateUrl: 'welcome.html'
 })
 export class WelcomePage {
 
-  constructor(public navCtrl: NavController) {}
+	constructor(public navCtrl: NavController) {
+	}
 
-  login() {
-    this.navCtrl.push(LoginPage);
-  }
+	public login(): Promise<any> {
+		return this.navCtrl.push(LoginPage);
+	}
 
-  signup() {
-    this.navCtrl.push(SignupPage);
-  }
+	public signup(): Promise<any> {
+		return this.navCtrl.push(SignupPage);
+	}
 }
